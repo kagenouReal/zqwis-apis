287a8c64bb675c7a2071a895808017515a1b302f
Kage sop spoken <92816209+kagenouReal@users.noreply.github.com>
2025-10-23 03:04:06 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ global.username = "kagenou ganteng";
 global.password = "zstres";
 global.apikey = "ZstresOwn01";
 global.port = "8080";
-global.domain = "" || `http://localhost:${global.port}`
+global.domain = "https://zqwis-apis.up.railway.app" || `http://localhost:${global.port}`
 //==========
 global.limitConfig = {
 user: 15,
